a6ecda37581ba9e6b28ad2262c50bbba764e97da
michelle <michelle_lee100@hotmail.com>
2022-11-11 17:47:10 -0500

    ufo.jsx

---
+++ connect-gatech/src/components/canvas/Ufo.jsx
@@ -0,0 +1,122 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+
+export function Ufo(props) {
+  const { nodes, materials } = useGLTF("models/ufo.gltf");
+  return (
+    <group {...props} dispose={null}>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.littleglasses.geometry}
+        material={nodes.littleglasses.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.ringbottom.geometry}
+        material={nodes.ringbottom.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.glass.geometry}
+        material={nodes.glass.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.ringtop.geometry}
+        material={nodes.ringtop.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.light.geometry}
+        material={nodes.light.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.saucer.geometry}
+        material={nodes.saucer.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.littleglasses001.geometry}
+        material={nodes.littleglasses001.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.ringbottom001.geometry}
+        material={nodes.ringbottom001.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.glass001.geometry}
+        material={nodes.glass001.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.ringtop001.geometry}
+        material={nodes.ringtop001.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.light001.geometry}
+        material={nodes.light001.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.saucer001.geometry}
+        material={nodes.saucer001.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.littleglasses002.geometry}
+        material={nodes.littleglasses002.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.ringbottom002.geometry}
+        material={nodes.ringbottom002.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.glass002.geometry}
+        material={nodes.glass002.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.ringtop002.geometry}
+        material={nodes.ringtop002.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.light002.geometry}
+        material={nodes.light002.material}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes.saucer002.geometry}
+        material={nodes.saucer002.material}
+      />
+    </group>
+  );
+}
+
+useGLTF.preload("/models/ufo.gltf");
+
+export default Ufo;

d4a87fb919994e9ee3661773de7cbf4bfb097320
yrmi99 <113481133+yrmi99@users.noreply.github.com>
2022-11-29 18:09:09 -0500

    Update buildingInfo.jsx

--- connect-gatech/src/database/buildingInfo.jsx
+++ connect-gatech/src/database/buildingInfo.jsx
@@ -3,8 +3,8 @@ const buildingInfo = [
         key: 1,
         id: "culc",
         title: "Clough Undergraduate Learning Commons",
-        desc: "The building serves as an interdisciplinary facility to encourage collaboration and technologically enhanced teaching and learning.[1][2] The Clough Commons, which was built with LEED certification in mind, contains various sustainability features and a roof garden, demonstrating Georgia Tech's commitment to sustainable design.[1] In August 2012, the building, staged as a building within Googleplex, was a major piece of the set of the 2013 film The Internship.",
-        micowave: "This building has a microwave in Room 284: Second floor CULC, requires Buzzcard access (possibly faculty only), locked after hours",
+        desc: "The building serves as an interdisciplinary facility to encourage collaboration and technologically enhanced teaching and learning. The Clough Commons, which was built with LEED certification in mind, contains various sustainability features and a roof garden, demonstrating Georgia Tech's commitment to sustainable design. In August 2012, the building, staged as a building within Googleplex, was a major piece of the set of the 2013 film The Internship.",
+        microwave: "This building has a microwave in Room 284: Second floor CULC, requires Buzzcard access (possibly faculty only), locked after hours",
         buzzcard: "Requires buzzcard access after 6pm",
         vending: "Has a vending machine on every floor",
     },
@@ -12,39 +12,98 @@ const buildingInfo = [
         key: 2,
         id: "techgreen",
         title: "Tech Green",
-        desc: "This is tech green!",
+        desc: "Tech Green is the great lawn of Georgia Tech and the gathering spot for 1000 attendees and hosts graduation ceremonies, festivals, Fairs, exhibits and displays, receptions and parties, athletic events, luncheons, outreach events, games, concerts and performances, races and walks, camp activities, and campus events.",
+        microwave: "N/A",
+        buzzcard: "N/A",
+        vending: "N/A",
     },
     {
         key: 3,
         id: "crc",
         title: "Campus Recreation Center",
         desc: "This is the CRC! You can play sports like basketball, volleyball, soccer, badminton, fencing, or just exercise. You can also rent sports equipment here.",
+        microwave: "N/A",
+        buzzcard: "Requires buzzcard to enter the gym",
+        vending: "Has a vending machine on the first floor near the cafe"
     }, 
     {
         key: 4,
         id: "klaus",
         title: "Klaus Advanced Computing Building",
         desc: "The building is home to the College of Computing's School of Computer Science and its School of Computational Science and Engineering, three research centers (GTISC, CERCS and ARC), and over 20 College of Computing Research Labs. It also houses much of the School of Electrical Engineering's computer engineering faculty and support staff. The binary bridge links this building to the College of Computing Building.",
-        micowave: "This building has two microwaves. One in the breakroom on the second and third floors near the giant stairs",
+        microwave: "This building has two microwaves. One in the breakroom on the second and third floors near the giant stairs",
         buzzcard: "Requires buzzcard access after 6pm",
         vending: "Has a vending machine on the first floor",
     },
-  {
-    key: 5,
-    id: "gilbert",
-    title: "Price Gilbert Memorial Library",
-    desc: "This is the main library of Georgia Tech. This is connected to Crosland Tower and the Clough Undergraduate Learning Commons, and is the main studying space for students.",
-    buzzcard: "Requires buzzcard access after 6pm",
-  },
-  {
+    {
+        key: 5,
+        id: "gilbert",
+        title: "Price Gilbert Memorial Library",
+        desc: "This is the main library of Georgia Tech. This is connected to Crosland Tower and the Clough Undergraduate Learning Commons, and is the main studying space for students.",
+        microwave: "N/A",
+        buzzcard: "Requires buzzcard access after 6pm",
+        vending: "N/A",
+    },
+      {
     key: 6,
     id: "crosland",
     title: "Crosland Tower",
     desc: "This is part of the Georgia Tech's main library. It is connected to Price Gilbert and contains quiet study area unlike Gilbert library.",
     buzzcard: "Requires buzzcard access after 6pm",
     vending: "Has a vending machine on the second, third and sixth floor",
-  }
-    
+  },
+  
+  {
+    key: 7,
+    id: "sc",
+    title: "John Lewis Student Center",
+    desc: "The Student Center houses 11 dining concepts as well as a post office, bowling alley, two theaters, 8 meeting spaces, study spaces, and the Student Organization Hub.",			
+    micowave: "This building has a microwave on the 2nd floor near Blue Donkey",
+    buzzcard: "Requires buzzcard access after 11pm",
+    food: "First floor food options include Chick-Fil-A, Panda Express, and There Burger Bar. Second floor food options include Blue Donkey, Tech it To Go, Campus Crust, Gryo Chef Mediterranean, Brain Freeze, Bento Sushi, 5th District Deli, and Test Kitchen.",
+    vending: "Has vending machines on each floor",
+  },
+  {
+    key: 8,
+    id: "stamps",
+    title: "Stamps Health Services",
+    desc: "Stamps Health Center is the on-campus outpatient ambulatory care center that provides health care and education to students.",
+    vending: "Has vending machines",
+   },
+
+
+    {
+      key: 9,
+      id: "skiles",
+      title: "Skiles Classroom Building",
+      desc: "The School of Mathematics is located on the first and second floor of this building. The LMC Office is located in Skiles 336. Most faculty, staff, and undergraduate advisor offices are also located at Skiles. This building is kinda spooky.",
+      microwave: "N/A",
+      buzzcard: "Does not require buzzcard access but is locked after 8pm",
+      vending: "not sure...",
+   },
+
+   {
+    key: 10,
+    id: "vanleer",
+    title: "Van Leer Building",
+    desc: "ECE administrative offices, classrooms, instructional and research labs (VIPs), and offices are located here. It is connected to the Interdisciplinary Design Commons which is a 15,000-square-foot, state-of-the-art makerspace—the largest of its kind in the nation—that caters specifically to electronics and computer engineering design and innovation. However, students of all majors can use the IDC, which like other makerspaces on campus offers them a hands-on learning environment.",
+    microwave: "This building has a microwave on the fifth floor but is intended for emag research",
+    buzzcard: "Requires buzzcard access after 6pm",
+    vending: "N/A",
+  },
+  {
+    key: 13,
+    id: "brittain",
+    title: "Brittain Dining Hall",
+    desc: "Brittain Dining Hall is one of three dining halls at Georgia Tech. It is located on east part of campus and known for its Gothic architecture which is rare at GT. But they have the least food options out of all dining halls.",
+    buzzcard: "Requires buzzcard access at all times. Building opens from 7:00AM to 10PM on weekdays only.",
+  },
+  {
+    key: 14,
+    id: "nav",
+    title: "North Avenue Dining Hall",
+    desc: "North Avenue Dining Hall, a.k.a. Nav, is located on the easternmost part of campus. It is not really spacious, so on weekends and nights, seats are mostly taken.
+    buzzcard: "Requires buzzcard access at all times. Building opens from 7:00AM to 10:PM on weekdays and 9:00AM to 9:00PM during weekends.",
 
 ];
 
